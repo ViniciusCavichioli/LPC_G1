14e96e5006fa7af4faee566114b26ea1c7198239
Vinicius Cavichioli <vinicavi@hotmail.com>
2017-09-25 21:04:56 -0300

    Trabalho G1 parte 2

--- lpc_calendario/agenda/admin.py
+++ lpc_calendario/agenda/admin.py
@@ -4,4 +4,5 @@ from agenda.models import *
 admin.site.register(Compromisso)
 admin.site.register(Agenda)
 admin.site.register(Compartilhamento)
+admin.site.register(Convite)
 # Register your models here.
--- lpc_calendario/agenda/models.py
+++ lpc_calendario/agenda/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Agenda(models.Model):
-    visivel = models.BooleanField(True)
+    visivel = models.BooleanField('Visivel')
     descricao = models.TextField()
     tipo = models.CharField(max_length = 100)
-    institucional = models.BooleanField(True)
+    institucional = models.BooleanField('institucional')
     usuario = models.ManyToManyField(User, null=True, blank=False)
 
     def __str__(self):
@@ -23,7 +23,11 @@ class Compromisso(models.Model):
 class Compartilhamento(models.Model):
     agenda = models.ForeignKey(Agenda, null=True, blank=False)
     usuario = models.ForeignKey(User, null=True, blank=False)
-    compromissos = models.ForeignKey(Compromisso, null=True, blank=False)
 
     def __str__(self):
         return "{}".format(self.agenda.tipo)
+
+class Convite(models.Model):
+    convidado = models.ManyToManyField(User, null=True, blank=False)
+    compromissos = models.ForeignKey(Compromisso, null=True, blank=False)
+    resposta = models.CharField(max_length = 50)
